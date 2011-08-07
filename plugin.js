487ba7945260b477446d190e812b9a6cc445793b
Shane Daniel <shane@luminopolis.com>
2011-08-07 04:01:37 -0500

    cleaning up indentation etc.

--- plugin.js
+++ plugin.js
@@ -20,34 +20,34 @@ jQuery(document).ready(function() {
         }
     }
 
-    span = jQuery('h3 span');
+    var span = jQuery('h3 span');
     
     if ('0' != span.length) {
-		//theme editor
-		file = span.html();
-		file = file.replace( /\(|\)/g, '');
-	}
-	else {
-		//plugin editor
-		file = jQuery('input[name="file"]').val();
-	}
-	file = file.split('.');
-	mode = file[1];
-	if (mode != 'php') mode = 'css';
-
-    var textarea = document.getElementById("newcontent");
-    if (textarea) {
-        CodeMirror.fromTextArea(document.getElementById("newcontent"), {
-            theme: "elegant",
-            mode: mode,
-            lineNumbers: true,
-            onKeyEvent: function(i, e) {
-                // Hook into F11
-                if ((e.keyCode == 122 || e.keyCode == 27) && e.type == "keydown") {
-                    e.stop();
-                    return toggleFullscreenEditing();
-                }
-            },
-        });
+      //theme editor
+      var file = span.html();
+      file = file.replace( /\(|\)/g, '');
+    } else {
+      //plugin editor
+      file = jQuery('input[name="file"]').val();
     }
+  file = file.split('.');
+  mode = file[1];
+  if (mode != 'php') 
+    mode = 'css';
+  
+  var textarea = document.getElementById("newcontent");
+  if (textarea) {
+    CodeMirror.fromTextArea(document.getElementById("newcontent"), {
+      theme: "elegant",
+      mode: mode,
+      lineNumbers: true,
+      onKeyEvent: function(i, e) {
+        // Hook into F11
+        if ((e.keyCode == 122 || e.keyCode == 27) && e.type == "keydown") {
+          e.stop();
+          return toggleFullscreenEditing();
+        }
+      },
+    });
+  }
 });
